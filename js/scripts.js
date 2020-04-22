ee0de28ee1886f90996bb832b0cb70039c1d9d80
Henry Javangwe <henrysparks1@gmail.com>
2020-04-22 17:26:35 +0200

    created an array of objects. each object contains data for a specific course. created a function that will set localStorage numbers to the total number of times the buttons with eventListener are clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,37 @@
 console.log (" the script is connected and running");
 let addToCart = document.querySelectorAll('#add-to-inquiry');
-// console.log(addToCart);
+
+let courses = [
+    {
+        name    : 'Become a Full Stack Web Developer ',
+        tag     : 'webdeveloper',
+        price   : 26000,
+        inCart  : 0
+    },
+    {
+        name    : 'Become a Front End Web Developer ',
+        tag     : 'frontend-dev',
+        price   : 9000,
+        inCart  : 0
+    },
+    {
+        name    : 'Become a Java Spring Boot Developer',
+        tag     : 'spring-boot-dev',
+        price   : 16000,
+        inCart  : 0
+    },
+];
+
+// looping through the array of nodeList since querySelector all returns an array of all the instances where that selector is found.
 for ( let i =0; i< addToCart.length; i++){
-    console.log('my loop');
+   addToCart[i].addEventListener('click', ($event)=>{
+       $event.preventDefault();
+       cartNumbers();
+   })
+//Writing a function that will set localStorage numbers to the total number of times the buttons with eventListener are clicked.
+   function cartNumbers(){
+       let courseNumbers= localStorage.getItem('cartNumbers');//we are accessing the local storage and grabbing the contents.
+       courseNumbers = parseInt(courseNumbers); //Numbers from the localStorage come as strings so we have to convert them to integers hence the parseInt;  
+       localStorage.setItem ('cartNumbers', 1);
+   }
 };
