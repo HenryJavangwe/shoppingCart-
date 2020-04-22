f4eaae8faf91b5bfd58f963c0932ba0459cf3ef8
Henry Javangwe <henrysparks1@gmail.com>
2020-04-22 16:51:14 +0200

    added 3 cards to the index page, all wrapped inside a new section inside a div. each card has an add to cart button and that's where we'll be working with to easily has access to those buttons and checking to see if our program is coming along the right way when developing.

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+console.log (" the script is connected and running");
+let addToCart = document.querySelectorAll('#add-to-inquiry');
+// console.log(addToCart);
+for ( let i =0; i< addToCart.length; i++){
+    console.log('my loop');
+};
